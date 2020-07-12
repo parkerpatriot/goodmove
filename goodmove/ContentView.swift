0d42c4e8cdb035776b0c2b6c78de65244dd2d32e
James Parker <parkerpatriot@gmail.com>
2020-07-11 20:11:27 -0600

    Initial Commit

---
+++ goodmove/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  goodmove
+//
+//  Created by James Parker on 7/11/20.
+//  Copyright © 2020 blackargyle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ goodmoveTests/goodmoveTests.swift
@@ -0,0 +1,34 @@
+//
+//  goodmoveTests.swift
+//  goodmoveTests
+//
+//  Created by James Parker on 7/11/20.
+//  Copyright © 2020 blackargyle. All rights reserved.
+//
+
+import XCTest
+@testable import goodmove
+
+class goodmoveTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
